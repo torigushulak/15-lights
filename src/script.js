53cf193dec9df600d78a67634b8113582a9a5184
Tori Gushulak <tori.gushulak@gmail.com>
2023-04-25 22:32:47 -0600

    lights done

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 /**
  * Base
@@ -16,17 +17,67 @@ const scene = new THREE.Scene()
 
 /**
  * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
+ */ 
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+// (color, intensity )
+// minimal cost
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight)
+// gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
+
+// (color, intensity )
+// moderate cost
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+
+// (sky color, ground color, intensity)
+// minimal cost
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+// (color, intensity, distance, decay )
+// moderate cost
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
 
-/**
+// (color, intensity, width, height)
+// 
+// only works with MeshStandardMaterial and MeshPhysicalMaterial
+// high cost
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1 );
+rectAreaLight.position.set(- 1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+// (color, intensity, distance, angle, penumbra, decay)
+// high cost
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 6, Math.PI * 0.1, 0.25, 1 )
+spotLight.position.set(0,2,3)
+scene.add(spotLight)
+
+spotLight.target.position.x = -0.75;
+scene.add(spotLight.target)
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+
+// has to be imported
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
+
+/** 
  * Objects
  */
 // Material
